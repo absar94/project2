6af2a0a539423f3fb5c88f03d6b6aca4b7940915
absar94 <31785257+absar94@users.noreply.github.com>
2017-09-26 14:55:34 -0500

    Create UserInfo.java
    Class will be called during the creation of the user account for Eventbrite.com

---
+++ src/eventBrite/UH/UserInfo.java
@@ -0,0 +1,13 @@
+package eventBrite.UH;
+
+public class UserInfo{
+
+	private String uName; 
+	private String pWord;
+
+public UserInfo(String username, String password) { //constructor
+
+		uName = username;
+		pWord = password;
+	}
+}
