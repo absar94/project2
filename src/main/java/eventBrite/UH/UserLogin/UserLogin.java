9cb836503e0e85875fc311a71d27e29e4063c696
absar94 <31785257+absar94@users.noreply.github.com>
2017-10-12 15:03:07 -0500

    Update : UserLogin.java
    Made the user login page

--- src/main/java/eventBrite/UH/UserLogin/UserLogin.java
+++ src/main/java/eventBrite/UH/UserLogin/UserLogin.java
@@ -1,4 +1,30 @@
 package eventBrite.UH.UserLogin;
+package eventBrite.UH.UserAccount.SignUp;
+package eventBrite.UH.UserAccount.UserInfo;
+import java.util.Scanner;
 
 public class UserLogin {
+  public static void LoginPage(){
+      
+      Scanner input = new Scanner(System.in); 
+      System.out.println("++++++++++++++++++++");
+    
+      System.out.print("Enter Username: ");
+      String uName = input.next();
+      System.out.print("Enter FirstName: ");
+      String fName = input.next();
+      System.out.print("Enter LastName: ");   
+      String lName = input.next();
+      System.out.print("Enter Password: "); 
+      String pWord = input.next();
+    
+      System.out.println("++++++++++++++++++++");
+        
+       if(check.auth()) 
+        System.out.println("You are logged in");
+
+
+    
+  }
+  
 }
